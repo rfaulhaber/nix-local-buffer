b1f95739b59fa1b40d8ea69ada7fc6bd72d70dd8
Ryan Faulhaber <ryf@sent.as>
2022-09-14 13:53:25 -0400

    basic functionality works

--- nix-local-buffer.el
+++ nix-local-buffer.el
@@ -19,6 +19,8 @@
 ;;
 ;;; Code:
 
+(require 'subr-x)
+
 ;; TODO optionally, run nix search, display list of packages
 ;; TODO default to nixpkgs flake, but optionally allow for different flake
 
@@ -28,11 +30,12 @@
   :prefix "nix-local-buffer-")
 
 (defconst nix-local-buffer-process-buffer-name "*nix build*")
+(defconst nix-local-buffer-process-error-buffer-name "*nix build error*")
 
 (defcustom nix-local-buffer-directory-name
-  default-directory
-  "Directory to place symbolic link for build result. Set to nil to pass the
---no-link flag to nix build."
+  "nix-local-buffer"
+  "Directory name to pass to --out-link.
+Set to nil to pass --no-link to nix build instead."
   :group 'nix-local-buffer
   :type '(directory))
 
@@ -41,6 +44,30 @@
   "Default flake to preface all arguments from. Also the default prompt for `nix-local-buffer'."
   :group 'nix-local-buffer
   :type '(string))
+
+(defun nix-local-buffer-get-output-links ()
+  (seq-map (lambda (path)
+             (concat path "/bin"))
+           (seq-filter (lambda (item)
+                         (not (string-empty-p item)))
+                       (split-string
+                        (with-current-buffer nix-local-buffer-process-buffer-name
+                          (buffer-string)) "\n"))))
+
+(defun nix-local-buffer-create-new-path (out-paths)
+  (let* ((path (parse-colon-path (getenv "PATH"))))
+    (concat "PATH=" (string-join (append path out-paths) ":"))))
+
+(defun nix-local-buffer-build-sentinel (process event)
+  (message "process: %s event %s" process event)
+  (when (string-match-p "finished" event)
+    (let* ((output-links (nix-local-buffer-get-output-links))
+           (new-path (nix-local-buffer-create-new-path output-links)))
+      (setq-local process-environment (append (seq-filter (lambda (item)
+                                                            (not (string-match-p (rx bol "PATH=") item))) process-environment)
+                                              (list new-path)))
+      (setq-local exec-path (append (default-value 'exec-path) output-links))))
+  (kill-buffer nix-local-buffer-process-buffer-name))
 
 ;;;###autoload
 (defun nix-local-buffer (flake pkgs)
@@ -63,11 +90,10 @@
     (make-process
      :name "nix build"
      :buffer nix-local-buffer-process-buffer-name
-     :command `("nix" "build" "--print-out-paths" ,@out-path-flag)
+     :command `("nix" "build" "--print-out-paths" ,@out-path-flag ,@installable-list)
      :noquery t
-     ;; TODO make error buffer
-     )
-    ))
+     :sentinel #'nix-local-buffer-build-sentinel
+     :stderr (get-buffer-create nix-local-buffer-process-error-buffer-name))))
 
 (provide 'nix-local-buffer)
 ;;; nix-local-buffer.el ends here
